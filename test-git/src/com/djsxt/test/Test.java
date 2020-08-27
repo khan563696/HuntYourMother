2ae6cdc4e40518af48cf0ef70611d9418954f514
Lenovo <Lenovo@DESKTOP-ORS7JMS>
2020-08-27 20:41:39 +0800

    commit interface and create test

---
+++ test-git/src/com/djsxt/test/Test.java
@@ -0,0 +1,24 @@
+package com.djsxt.test;
+
+public class Test implements TestInterface
+{
+
+	@Override
+	public void m() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void n() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void c() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
